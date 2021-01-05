3a5a03014ffd30d826bb6e687bae3c6cdca6bbbf
BenjaminOsojnik <benjamin.osojnik@gmail.com>
2021-01-05 07:47:01 +0100

    Lazy loading fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React, {Fragment, Suspense, lazy} from 'react'
 import './App.css'
 import BasicResumeForm from './forms/BasicResumeForm'
 import { BrowserRouter as Router, Route,Switch } from 'react-router-dom'
+import AdvancedResumeForm from './forms/AdvancedResumeForm'
 
-const AdvancedResumeForm = lazy(()=> import('./forms/AdvancedResumeForm'))
-const Export2PdfPage = lazy(() => import('./pages/Export2PdfPage') )
+const Export2PdfPage = lazy(() => import('./pages/Export2PdfPage'))
 
 function App() {
   return (
@@ -12,11 +12,15 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={BasicResumeForm} />
-          <Suspense fallback={<div />}>
-            <Route exact path="/advanced" component={AdvancedResumeForm} />
-          </Suspense>
-          <Suspense fallback={<div />}>
-          <Route exact path="/export2pdf" component={Export2PdfPage} />
+          <Route exact path="/advanced" component={AdvancedResumeForm} />
+          <Suspense fallback={<div className="main-wrapper">
+            <div className="main">
+                <div id="preview">
+                    <h1 id="title">Getting things ready..</h1>
+                </div>
+            </div>
+        </div>}>
+            <Route exact path="/export2pdf" component={Export2PdfPage} />
           </Suspense>
           <Route component={BasicResumeForm} />
         </Switch>
